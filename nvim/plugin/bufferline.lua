c63a3a7697471354d6eb4d4490094b707ff6b79b
cooparo <github.ambitious226@slmails.com>
2025-03-27 13:16:31 +0100

    bufferline moved keymaps to specific file

--- nvim/plugin/bufferline.lua
+++ nvim/plugin/bufferline.lua
@@ -3,10 +3,6 @@ if vim.g.did_load_bufferline_plugin then
 end
 vim.g.did_load_bufferline_plugin = true
 
---Bufferline
-vim.keymap.set("n", "<Tab>", "<cmd>BufferLineCycleNext<CR>")
-vim.keymap.set("n", "<S-Tab>", "<cmd>BufferLineCyclePrev<CR>")
-
 require('bufferline').setup {
 	options = {
 		separator_style = "thick",
