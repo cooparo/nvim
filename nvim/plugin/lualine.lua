82612c0b3c5db3cc9bed39b4c40a36d5dc826406
cooparo <github.ambitious226@slmails.com>
2025-03-27 15:30:18 +0100

    added lualine symbol for recording/playing macros

--- nvim/plugin/lualine.lua
+++ nvim/plugin/lualine.lua
@@ -1,5 +1,5 @@
 if vim.g.did_load_lualine_plugin then
-	return
+  return
 end
 vim.g.did_load_lualine_plugin = true
 
@@ -9,62 +9,62 @@ navic.setup {}
 ---Indicators for special modes,
 ---@return string status
 local function extra_mode_status()
-	-- recording macros
-	local reg_recording = vim.fn.reg_recording()
-	if reg_recording ~= '' then
-		return ' @' .. reg_recording
-	end
-	-- executing macros
-	local reg_executing = vim.fn.reg_executing()
-	if reg_executing ~= '' then
-		return ' @' .. reg_executing
-	end
-	-- ix mode (<C-x> in insert mode to trigger different builtin completion sources)
-	local mode = vim.api.nvim_get_mode().mode
-	if mode == 'ix' then
-		return '^X: (^]^D^E^F^I^K^L^N^O^Ps^U^V^Y)'
-	end
-	return ''
+  -- recording macros
+  local reg_recording = vim.fn.reg_recording()
+  if reg_recording ~= '' then
+    return ' @' .. reg_recording
+  end
+  -- executing macros
+  local reg_executing = vim.fn.reg_executing()
+  if reg_executing ~= '' then
+    return ' @' .. reg_executing
+  end
+  -- ix mode (<C-x> in insert mode to trigger different builtin completion sources)
+  local mode = vim.api.nvim_get_mode().mode
+  if mode == 'ix' then
+    return '^X: (^]^D^E^F^I^K^L^N^O^Ps^U^V^Y)'
+  end
+  return ''
 end
 
 require('lualine').setup {
-	options = {
-		icons_enabled = true,
-		theme = 'auto',
-		component_separators = { left = '', right = '' },
-		section_separators = { left = '', right = '' },
-		disabled_filetypes = {
-			statusline = { 'neo-tree', 'NvimTree' },
-			winbar = {},
-		},
-		ignore_focus = {},
-		always_divide_middle = true,
-		always_show_tabline = true,
-		globalstatus = false,
-		refresh = {
-			statusline = 100,
-			tabline = 100,
-			winbar = 100,
-		}
-	},
-	sections = {
-		lualine_a = { 'mode' },
-		lualine_b = { 'branch', 'diff', 'diagnostics' },
-		lualine_c = { 'filename' },
-		lualine_x = { 'encoding', 'fileformat', 'filetype' },
-		lualine_y = { 'progress' },
-		lualine_z = { 'location' }
-	},
-	inactive_sections = {
-		lualine_a = {},
-		lualine_b = {},
-		lualine_c = { 'filename' },
-		lualine_x = { 'location' },
-		lualine_y = {},
-		lualine_z = {}
-	},
-	tabline = {},
-	winbar = {},
-	inactive_winbar = {},
-	extensions = {}
+  options = {
+    icons_enabled = true,
+    theme = 'auto',
+    component_separators = { left = '', right = '' },
+    section_separators = { left = '', right = '' },
+    disabled_filetypes = {
+      statusline = { 'neo-tree', 'NvimTree' },
+      winbar = {},
+    },
+    ignore_focus = {},
+    always_divide_middle = true,
+    always_show_tabline = true,
+    globalstatus = false,
+    refresh = {
+      statusline = 100,
+      tabline = 100,
+      winbar = 100,
+    }
+  },
+  sections = {
+    lualine_a = { 'mode' },
+    lualine_b = { 'branch', 'diff', 'diagnostics' },
+    lualine_c = { 'filename', extra_mode_status },
+    lualine_x = { 'encoding', 'fileformat', 'filetype' },
+    lualine_y = { 'progress' },
+    lualine_z = { 'location' }
+  },
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = { 'filename' },
+    lualine_x = { 'location' },
+    lualine_y = {},
+    lualine_z = {}
+  },
+  tabline = {},
+  winbar = {},
+  inactive_winbar = {},
+  extensions = {}
 }
